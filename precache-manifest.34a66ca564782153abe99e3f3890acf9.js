2c5d0758e0223485f11ab24fd2bc6fb44818a7b7
Ankit Baranwal <akbaranwal22@gmail.com>
2020-12-09 17:14:58 +0530

    Updates

---
+++ precache-manifest.34a66ca564782153abe99e3f3890acf9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c94d95db063c129ce91ce2e1bb1fa6e3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d9ea01ff906d3382604a",
+    "url": "./static/css/main.8378bf1d.chunk.css"
+  },
+  {
+    "revision": "29ea514b03c1dfc7a337",
+    "url": "./static/js/2.809d901d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.809d901d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9ea01ff906d3382604a",
+    "url": "./static/js/main.b461190f.chunk.js"
+  },
+  {
+    "revision": "658601d5d1b3c46cea23",
+    "url": "./static/js/runtime-main.2d3c87ab.js"
+  }
+]);
